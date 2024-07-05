65c3aff2cbd63c9275a2e832fa06ac0c184a206a
ansumanpatro11 <132783376+ansumanpatro11@users.noreply.github.com>
2024-07-05 18:29:00 +0530

    Update setup.py

--- ros_task_2/bot_control/setup.py
+++ ros_task_2/bot_control/setup.py
@@ -7,6 +7,7 @@ package_name = 'bot_control'
 setup(
     name=package_name,
     version='0.0.0',
+    packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
         ['resource/' + package_name]),
